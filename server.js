3b20a71a76bab20e9b1c61869a34f7f43bfaee94
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-05-12 23:19:36 +0200

    next

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/heroku-front'));
+app.use(
+  express.static(__dirname + '/dist/heroku-front')
+  );
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/heroku-front/index.html'));
+  res.sendFile(
+    path.join(__dirname+'/dist/heroku-front/index.html')
+    );
 });
 
 // Start the app by listening on the default Heroku port
