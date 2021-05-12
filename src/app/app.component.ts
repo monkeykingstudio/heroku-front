38cef75086b8bad01b2cbe26869a08c12e80026a
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-05-12 15:13:41 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { AuthService } from './services/auth.service';
+// import { User } from './models/user.model';
+import { Subscription } from 'rxjs';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'heroku-front';
+export class AppComponent implements OnInit {
+  public authStatusSubscription: Subscription;
+
+  title = 'antslab';
+  hover = false;
+  isVerified = false;
+
+  // user$: Observable<User>;
+  // currentUser: User;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // this.authStatusSubscription = this.authService.currentUser.pipe(
+    //   map(user => {
+    //     if (user) {
+    //       this.currentUser = user;
+    //     }
+    //   })
+    //   ).subscribe();
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
