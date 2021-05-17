6c142927885c9aa96ddd2667b13966741eace5b9
raphael <raphael.vareilles@gmail.com>
2021-05-18 00:52:50 +0200

    fix form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -10,7 +11,6 @@ import { ErrorInterceptor } from './auth/error-interceptor';
 import { HeaderComponent } from './header/header.component';
 import { LateralMenuComponent } from './lateral-menu/lateral-menu.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PopupModule } from './popup/popup.module';
 import { ColonyCardComponent } from './colonies/colony-card/colony-card.component';
 import { ColoniesListComponent } from './colonies/colonies-list/colonies-list.component';
