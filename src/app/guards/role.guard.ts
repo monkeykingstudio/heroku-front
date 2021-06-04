75a38573f2a800619dc5569dca99015fd827bcf6
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-06-04 11:02:35 +0200

    role guard 2

---
+++ src/app/guards/role.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  storage;
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot):
+    boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    const isAuth = this.authService.getIsAuth;
+    if (isAuth) {
+      this.storage = JSON.parse(localStorage.getItem('currentUser'));
+
+      if (!isAuth) {
+        this.router.navigate(['/login']);
+        return false;
+      } else if (isAuth && this.storage.role === 'admin') {
+        this.router.navigate(['/breedingsheet']);
+        return true;
+      }
+      else if (isAuth && this.storage.role !== 'admin') {
+        this.router.navigate(['']);
+        return false;
+      }
+      else {
+        this.router.navigate(['']);
+        return false;
+      }
+    }
+    return false;
+    this.router.navigate(['/login']);
+  }
+
+}
