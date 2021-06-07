c77bfabaeeac17db9450d7117daf7735f283730c
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-06-08 01:35:33 +0200

    lateral menu

--- src/app/lateral-menu/lateral-menu.component.ts
+++ src/app/lateral-menu/lateral-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lateral-menu',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lateral-menu.component.scss']
 })
 export class LateralMenuComponent implements OnInit {
-  hover;
+  @Input()
+  hoverStatus;
 
   constructor() { }
 
