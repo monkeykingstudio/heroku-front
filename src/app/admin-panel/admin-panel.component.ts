5aecd8d27be43e0996294fe5ef8d8d6deebf9902
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-06-10 00:25:22 +0200

    admin pending users

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -4,6 +4,8 @@ import { UsersService } from './../services/user.service';
 import { User } from './../models/user.model';
 import { BreedingSheetsService } from './../services/breedingSheetsService';
 import { ColoniesService } from './../services/colonies.service';
+import { BreedingSheet } from './../models/breedingSheet.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-panel',
@@ -12,12 +14,22 @@ import { ColoniesService } from './../services/colonies.service';
 })
 export class AdminPanelComponent implements OnInit {
   allUsers$: Observable<User[]>;
+  pendingUsers$: Observable<User[]>;
 
-
-  constructor(public usersService: UsersService) { }
+  constructor(
+    public usersService: UsersService,
+    public coloniesService: ColoniesService,
+    public breedingSheetsService: BreedingSheetsService
+    ) { }
 
   ngOnInit(): void {
     this.reloadUsers();
+
+    this.pendingUsers$ = this.allUsers$
+    .pipe(
+      map(users => users
+        .filter(user => user.is_verified === false))
+    );
   }
 
   reloadUsers(): void {
