4eb3efc9867528d688441bced9a6e6ba9a122f74
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-06-20 23:10:58 +0200

    delete breedsheet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'adminpanel', component: AdminPanelComponent, canActivate: [RoleGuard, AuthGuard] },
-  { path: 'adminpanel/breedsheetcreator', component: BreedSheetCreatorComponent, canActivate: [RoleGuard, AuthGuard] },
+  { path: 'breedsheetcreator', component: BreedSheetCreatorComponent, canActivate: [AuthGuard] },
   { path: 'breedsheetviewer/:sheetId', component: BreedSheetViewerComponent, canActivate: [AuthGuard] },
   { path: 'colonies', component: ColoniesListComponent, canActivate: [AuthGuard]},
   { path: 'colonies/:colonyId', component: ColonyComponent,  canActivate: [AuthGuard]},
