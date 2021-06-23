f079ba0a2c85456e03f7833e07cc03ec6c8b342e
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-06-23 11:52:46 +0200

    users connected

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { UsersService } from './../services/user.service';
 import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +11,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  allUsers$: Observable<User[]>;
+  onlineUsers$: Observable<User[]>;
+
   height: number;
   width: number;
   @ViewChild('element', {read: ElementRef, static: false}) elementView: ElementRef;
   @ViewChild('same', {read: ElementRef, static: false}) elementSameView: ElementRef;
   @ViewChild('secondsame', {read: ElementRef, static: false}) elementSecondSameView: ElementRef;
 
-
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.resize();
   }
+
 
   constructor(
     public usersService: UsersService,
@@ -26,8 +32,22 @@ export class HomeComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
+    this.reloadUsers();
+    this.reloadOnlineUsers();
   }
 
+  reloadUsers(): void {
+    const users$ = this.usersService.usersGet();
+    this.allUsers$ = users$;
+  }
+
+  reloadOnlineUsers() {
+    this.onlineUsers$ = this.allUsers$
+    .pipe(
+      map(users => users
+        .filter(user => user.isConnected === true))
+    );
+  }
 
   ngAfterViewInit() {
     this.resize();
