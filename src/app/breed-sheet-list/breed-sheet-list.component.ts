d75e69366b213c8bebb69e489584fa09c0ba0a6c
raphael <raphael.vareilles@gmail.com>
2021-08-19 17:42:16 +0200

    apres midi cofrac

--- src/app/breed-sheet-list/breed-sheet-list.component.ts
+++ src/app/breed-sheet-list/breed-sheet-list.component.ts
@@ -21,8 +21,12 @@ export class BreedSheetListComponent implements OnInit, OnDestroy {
   allFamilies: string[] = [];
   sortedFamilies: string[] = [];
 
+  allSubFamilies: string[] = [];
+  sortedSubFamilies: string[] = [];
+
   private sheetSub: Subscription;
   private familySub: Subscription;
+  private subFamilySub: Subscription;
 
   constructor(
     public breedingSheetsService: BreedingSheetsService
@@ -67,14 +71,43 @@ export class BreedSheetListComponent implements OnInit, OnDestroy {
       for (const item of res) {
         this.allFamilies.push(item);
       }
-      const sortFamilies = this.familySort(this.allFamilies);
+      this.familyStore(this.allFamilies);
+    });
+
+    // All sub-families
+    this.subFamilySub = this.allSheets$
+    .pipe(
+      map(breedsheets => breedsheets
+        .filter(sheets => sheets.status === 'approved')
+        .map(sheet => sheet.subfamily)
+      ),
+      tap(res => {
+        res.sort(this.compareFn);
+      })
+    )
+    .subscribe((res) => {
+      for (const item of res) {
+        this.allSubFamilies.push(item);
+      }
+      this.subFamilyStore(this.allSubFamilies);
     });
   }
 
-  familySort(array) {
+  familyStore(array) {
     for (const item of array) {
       if (!this.sortedFamilies.includes(item.toLowerCase())) {
         this.sortedFamilies.push(item.toLowerCase());
+        this.sortedFamilies.sort();
+      }
+    }
+    return this.sortedFamilies;
+  }
+
+  subFamilyStore(array) {
+    for (const item of array) {
+      if (!this.sortedSubFamilies.includes(item.toLowerCase())) {
+        this.sortedSubFamilies.push(item.toLowerCase());
+        this.sortedSubFamilies.sort();
       }
     }
     return this.sortedFamilies;
@@ -121,6 +154,47 @@ export class BreedSheetListComponent implements OnInit, OnDestroy {
     }
   }
 
+  subFamilySelect(value) {
+    // LOAD ALL
+    if (value === 'all') {
+      this.sheetSub = this.allSheets$
+      .pipe(
+        map(
+          breedingSheet => breedingSheet
+          .filter(sheets => sheets.status === 'approved')
+        ),
+        tap(res => {
+          res.sort(this.compareFn);
+        })
+      )
+      .subscribe((res) => {
+        this.sortedSpecies = [];
+        res.forEach((value) => {
+          this.sortedSpecies.push(value);
+        });
+      });
+    } else {
+      // LOAD SELECTED
+      this.sheetSub = this.allSheets$
+      .pipe(
+        map(
+          breedingSheet => breedingSheet
+          .filter(sheets => sheets.status === 'approved')
+          .filter(sheet => sheet.subfamily === value)
+        ),
+        tap(res => {
+          res.sort(this.compareFn);
+        })
+      )
+      .subscribe((res) => {
+        this.sortedSpecies = [];
+        res.forEach((value) => {
+          this.sortedSpecies.push(value);
+        });
+      });
+    }
+  }
+
   reloadBreedingSheets(): void {
     const sheets$ = this.breedingSheetsService.getAll();
     this.allSheets$ = sheets$;
@@ -139,7 +213,7 @@ export class BreedSheetListComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.sheetSub.unsubscribe();
     this.familySub.unsubscribe();
-
+    this.subFamilySub.unsubscribe();
   }
 
 }
