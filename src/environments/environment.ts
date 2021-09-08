e824594e8ace8700322783bf09e1ae752bd12da3
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-09-09 00:40:34 +0200

    try socket notifications

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  APIEndpoint: 'http://localhost:8081'
-  // APIEndpoint: 'https://calm-waters-91692.herokuapp.com'
+  // APIEndpoint: 'http://localhost:8081'
+  APIEndpoint: 'https://calm-waters-91692.herokuapp.com'
 
 };
 
